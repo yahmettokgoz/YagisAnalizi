8a7f8b3db8b1f4f9b2a84040f1477ca6cf8e116c
hamitkorkmaz0344 <hakorkmaz.2003@gmail.com>
2024-10-24 00:36:31 +0300

    Update main.py
    update

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title('Yağış analiz uygulaması')
+
+st.markdown('Bu web sitesi ile belirlediğiniz tarih aralığındaki yağış verilerini inceleyebilir,grafikler ve tablolar üzerinde detaylı analiz yapabilirsiniz.')
+
+st.image("C:\\Users\\90551\\Pictures\\yağış.png",caption="Türkiye Yağış Haritası:",use_column_width=True)
