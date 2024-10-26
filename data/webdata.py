afcea41359f4f55715f6ae07def8da52e33f24b6
moztrk <117629976+moztrk@users.noreply.github.com>
2024-10-26 13:26:14 +0300

    update

--- data/webdata.py
+++ data/webdata.py
@@ -12,17 +12,17 @@ class RainfallData:
     def __init__(self):
         self.url = "https://www.mgm.gov.tr/sondurum/toplam-yagis.aspx"
         self.driver = webdriver.Chrome()
-        self.conn = self.connect_db()  # Veritabanına bağlan
+        self.conn = self.connect_db()  
         self.cursor = self.conn.cursor()
 
     def connect_db(self):
         try:
             conn = psycopg2.connect(
-                dbname='raindb',  # Veritabanı adı
-                user='postgres',    # Kullanıcı adı
-                password='134679',       # Şifre
-                host='localhost',        # Sunucu
-                port='5432'              # PostgreSQL varsayılan portu
+                dbname='raindb',  
+                user='postgres',    
+                password='134679',       
+                host='localhost',        
+                port='5432'              
             )
             print("Veritabanı bağlantısı başarılı")
             return conn
@@ -31,39 +31,39 @@ class RainfallData:
 
     def get_rainfall_data(self):
         self.driver.get(self.url)
-        time.sleep(7)  # Sayfanın yüklenmesi için geçici bekleme
+        time.sleep(7)  #sayfanın yüklenmesi için geçici bekleme
 
-        # Tabloyu bulmak için bekleme
+        #tabloyu bulmak için bekleme
         WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="cph_body_pnlTablo"]/table'))
         )
 
-        # Tabloyu bul
+        #tablo bulma
         table = self.driver.find_element(By.XPATH, '//*[@id="cph_body_pnlTablo"]/table')
         rows = table.find_elements(By.TAG_NAME, "tr")
 
-        # Şu anki tarihi al
-        current_date = datetime.now().strftime("%Y-%m-%d")  # Mevcut tarihi al
+        
+        current_date = datetime.now().strftime("%Y-%m-%d")  
 
-        for row in rows[1:]:  # Başlık satırını atla
+        for row in rows[1:]:  #ilk satırı atlama başlık olma durumu
             columns = row.find_elements(By.TAG_NAME, "td")
             if len(columns) > 0:
                 city_name = columns[0].text.strip() if len(columns) > 0 else None
                 
-                # Yağış verisini 1. sütundan alıyoruz
+                
                 rainfall_text = columns[1].text.strip() if len(columns) > 1 else '0'
                 
-                # Yağış miktarını ekrana basarak kontrol edelim
+                
                 print(f"Çekilen yağış verisi: {rainfall_text}")
 
                 # ',' varsa onu '.' ile değiştiriyoruz ve float'a çeviriyoruz
                 rainfall = float(rainfall_text.replace(',', '.')) if rainfall_text else 0.0
 
-                # Basitleştirilmiş veriyi ekle
+                #db ekleme 
                 self.insert_data(city_name, rainfall, current_date)
 
         self.driver.quit()
-        self.conn.close()  # Veritabanı bağlantısını kapat
+        self.conn.close()  
 
     def insert_data(self, city_name, rainfall, measurement_date):
         try:
@@ -72,11 +72,11 @@ class RainfallData:
             VALUES (%s, %s, %s);
             """
             self.cursor.execute(insert_query, (city_name, rainfall, measurement_date))
-            self.conn.commit()  # Değişiklikleri kaydet
+            self.conn.commit()  
             print(f"Veri eklendi: {city_name}, {rainfall}, {measurement_date}")
         except Exception as e:
             print("Veri eklerken hata:", e)
 
-# Kullanım
+
 rainfall_data = RainfallData()
 rainfall_data.get_rainfall_data()
